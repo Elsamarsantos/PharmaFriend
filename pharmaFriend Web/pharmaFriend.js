895d80501c24f88a47bf8bf22ab651e0f42129d6
Diogo <diogo.sousa23@outlook.com>
2018-12-17 16:18:38 +0000

    no message

---
+++ pharmaFriend Web/pharmaFriend.js
@@ -0,0 +1,11 @@
+myPharmacy.showProductList() {
+   console.log("Product List");
+}
+
+showPharmacyList() {
+    console.log ("Pharmacy List");
+}
+
+showOurVision() {
+    console.log("Our Vision");
+}
