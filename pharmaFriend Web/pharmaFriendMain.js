1d62bcc8b708685fd6f1300ac409b91a834c73f2
Diogo <diogo.sousa23@outlook.com>
2018-12-20 15:03:48 +0000

    20/12

--- pharmaFriend Web/pharmaFriendMain.js
+++ pharmaFriend Web/pharmaFriendMain.js
@@ -4,8 +4,4 @@
 $("#btnMainSearch").click(function mainSearch() {
     console.log("SEARCHING MEDICINE...")
     $('#homeOutput').modal('show');
-    $("#homeOutput").html("Based in your location " + $("#myLocation").val() + "<br> The medicine " +
-    $("#medicineName").val() + " is in those nearby pharmacies: <br>" + 
-    "Pharmacy Castelo [0.2KM] <br>" +  "Pharmacy Areias [4KM] <br>" +  "Pharmacy Varandas [23KM]");
-    
 });
