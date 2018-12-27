6abfcc13dafda721c1a0c634bf6e7d33be32496a
elsamarsantos@gmail.com <44403607+Elsamarsantos@users.noreply.github.com>
2018-12-27 00:08:16 +0000

    .

--- pharmaFriend Web/pharmaFriendMain.js
+++ pharmaFriend Web/pharmaFriendMain.js
@@ -20,8 +20,81 @@ $("#btnMainSearch").click(function mainSearch() {
             var longitude = position.coords.longitude;
             output.innerHTML = '<p>Latitude: ' + latitude + '° <br>Longitude: ' + longitude + '°</p>';
             var inputmedicine = $("#medicineName").val();
-            var distance= $("#userdistance").val();
+            var distance = $("#userdistance").val();
 
+
+
+
+            /*  ------------------------------------- MAP ----------------------------------------------- */
+
+            var map = L.map('map').setView([latitude, longitude], 17);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            /* ------------------------------------ MARKER --------------------------------------------*/
+
+
+
+            /*---------- Person Marker ---------- */
+            var personMarker = L.AwesomeMarkers.icon({
+                icon: 'street-view',
+                prefix: 'fa'
+            });
+
+            /*------------ Pharmacy Marker ------------ */
+            var pharmacyMarker = L.AwesomeMarkers.icon({
+                icon: 'plus-square',
+                prefix: 'fa',
+                spin: true
+            });
+
+            var pharmacyMarkeroff = L.AwesomeMarkers.icon({
+                icon: 'plus-square',
+                prefix: 'fa',
+
+            });
+
+
+            L.marker([latitude, longitude], { icon: personMarker }).addTo(map)
+                .bindPopup('You ! ')
+                .openPopup();
+
+            /* TESTE PHARMACY DOMUS MASSAMA */
+            L.marker([38.7540326, -9.2799262], { icon: pharmacyMarker }).addTo(map)
+                .bindPopup('Pharmacy ! <br> Nome: Domus ')
+            //  .openPopup();
+
+            /* TESTE PHARMACY ONEILL */
+            L.marker([38.7548488, -9.2754248], { icon: pharmacyMarker }).addTo(map)
+                .bindPopup('Pharmacy Oneill<br><br>Dispõe do medicamento pesquisado! ')
+            // .openPopup();
+
+            /* TESTE PHARMACY  PINTO LEAL*/
+            L.marker([38.7528643, -9.2843015], { icon: pharmacyMarker }).addTo(map)
+                .bindPopup('Pharmacy ! <br> Nome: Pinto Leal ')
+            // .openPopup();
+
+            /* TESTE PHARMACY QUINTA DAS FLORES*/
+            L.marker([38.7575282, -9.2804767], { icon: pharmacyMarkeroff }).addTo(map)
+                .bindPopup('Pharmacy ! <br> Nome: Quinta das Flores <br>Horário: Fechado')
+            //.openPopup();
+            /* ------------------------------- ADD PHARMACY -------------------------------------- 
+         
+                                Construtor de localização e MArcadores                            */
+
+            /* var nrPharmacy=x;
+             for (i=0, i==x, i++){
+             var pharmacylat = 
+           
+             L.marker([latitude, longitude]).addTo(map)
+             .bindPopup('Nome farmacia <br> TEXTO etc ')
+             .openPopup();
+             }
+             */
+            
+            /* ------------------------------- search medicine in pharmacies ------------------     */
             $.ajax({
 
                 url: `http://localhost:8080/pharmafriend/api/request?medicinename=${inputmedicine}&lonlocation=${longitude}&latlocation=${latitude}&userdistance=${distance}`,
@@ -31,99 +104,29 @@ $("#btnMainSearch").click(function mainSearch() {
                     'Content-Type': 'application/json'
                 },
                 success: function (data) {
-                    console.log("show"+ data);
+                    console.log("show" + data);
 
-                   
-                    
+                    for (i = 0; i < data.lenght; i++) {
+                        L.marker([data[i].latLocation, data[i].lonLocation], { icon: pharmacyMarker }).addTo(map)
+                            .bindPopup(data[i].pharmacyName + ' <br> ' + data[i].address)
+                    }
+
                 }
             })
-        
-
-
-        /*  ------------------------------------- MAP ----------------------------------------------- */
-
-        var map = L.map('map').setView([latitude, longitude], 17);
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        /* ------------------------------------ MARKER --------------------------------------------*/
-
-
-
-        /*---------- Person Marker ---------- */
-        var personMarker = L.AwesomeMarkers.icon({
-            icon: 'street-view',
-            prefix: 'fa'
-        });
-
-        /*------------ Pharmacy Marker ------------ */
-        var pharmacyMarker = L.AwesomeMarkers.icon({
-            icon: 'plus-square',
-            prefix: 'fa',
-            spin: true
-        });
-
-        var pharmacyMarkeroff = L.AwesomeMarkers.icon({
-            icon: 'plus-square',
-            prefix: 'fa',
-
-        });
-
-
-        L.marker([latitude, longitude], { icon: personMarker }).addTo(map)
-            .bindPopup('You ! ')
-            .openPopup();
-
-        /* TESTE PHARMACY DOMUS MASSAMA */
-        L.marker([38.7540326, -9.2799262], { icon: pharmacyMarker }).addTo(map)
-            .bindPopup('Pharmacy ! <br> Nome: Domus ')
-        //  .openPopup();
-
-        /* TESTE PHARMACY ONEILL */
-        L.marker([38.7548488, -9.2754248], { icon: pharmacyMarker }).addTo(map)
-            .bindPopup('Pharmacy Oneill<br><br>Dispõe do medicamento pesquisado! ')
-        // .openPopup();
-
-        /* TESTE PHARMACY  PINTO LEAL*/
-        L.marker([38.7528643, -9.2843015], { icon: pharmacyMarker }).addTo(map)
-            .bindPopup('Pharmacy ! <br> Nome: Pinto Leal ')
-        // .openPopup();
-
-        /* TESTE PHARMACY QUINTA DAS FLORES*/
-        L.marker([38.7575282, -9.2804767], { icon: pharmacyMarkeroff }).addTo(map)
-            .bindPopup('Pharmacy ! <br> Nome: Quinta das Flores <br>Horário: Fechado')
-        //.openPopup();
-        /* ------------------------------- ADD PHARMACY -------------------------------------- 
-     
-                            Construtor de localização e MArcadores                            */
-
-        /* var nrPharmacy=x;
-         for (i=0, i==x, i++){
-         var pharmacylat = 
-       
-         L.marker([latitude, longitude]).addTo(map)
-         .bindPopup('Nome farmacia <br> TEXTO etc ')
-         .openPopup();
-         }
-         */
-
-
 
         }
 
-    function error() {
-        output.innerHTML = "Unable to retrieve your location, please chek your internet connection";
-    }
+        function error() {
+            output.innerHTML = "Unable to retrieve your location, please chek your internet connection";
+        }
 
-    output.innerHTML = "<p>Getting Location...</p>";
-    navigator.geolocation.getCurrentPosition(success, error);
-}
-else {
+        output.innerHTML = "<p>Getting Location...</p>";
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+    else {
 
         alert("Please insert a medicine in search field");
-    let element = document.getElementById("btnMainSearch");
+        let element = document.getElementById("btnMainSearch");
         element.setAttribute('data-toggle', "");
     }
 });
